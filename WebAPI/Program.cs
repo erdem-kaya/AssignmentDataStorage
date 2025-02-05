06f90c5e542da367f6415e128a2f23bacf9b7140
Erdem Kaya <kerdem@live.com>
2025-02-05 13:22:17 +0100

    Start WebApi1

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,23 +1,21 @@
+using Business.Interfaces;
+using Data.Contexts;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\AssignmentDataStorage\Data\Databases\assignment_database.mdf;Integrated Security=True;Connect Timeout=30"));
+builder.Services.AddScoped<ICompanyRepository, ICompanyRepository>();
+builder.Services.AddScoped<ICompanyService, ICompanyService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.MapOpenApi();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
